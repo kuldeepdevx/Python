83e0915bdd7408dbff5c3d1cfc2ea1f0bb777421
kuldeepdevx <kuldeep.girase.work@gmail.com>
2026-03-13 09:40:03 +0530

    Add if-else code

---
+++ Basics/if-else.py
@@ -0,0 +1,13 @@
+A=int(input("Enter a Number:"))
+
+if A%2==0:
+ print("A is an even number")
+else:
+ print("A is an odd number")
+
+if A>0:
+ print("The Number is positive")
+elif A<0:
+ print("The Number is negative")
+else:
+ print("The Number is zero")
